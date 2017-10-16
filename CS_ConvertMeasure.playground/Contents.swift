bfda2d918a14567a2664ff6b8873f54e1768d910
conalls <comets195@gmail.com>
2017-10-16 16:16:01 +0900

    길이변환 및 출력 커밋.

---
+++ CS_ConvertMeasure.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let var1: Int = 120
+print("\(var1)cm")
+
+var convertvar1: Double = Double(var1)/100
+print("\(convertvar1)m")
+
+let var2: Float = 1.86
+print("\(var2)m")
+
+var convertvar2 = var2 * 100
+print("\(Int(convertvar2))cm")
