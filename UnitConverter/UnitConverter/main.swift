9dea0b852206d3a9604ab8de1a3d498040475003
conalls <comets195@gmail.com>
2017-10-17 15:54:09 +0900

    Level 2. Lecture 2. Lesson 5. Commit 3번

--- UnitConverter/UnitConverter/main.swift
+++ UnitConverter/UnitConverter/main.swift
@@ -2,19 +2,37 @@
 //  main.swift
 //  UnitConverter
 //
-//  Created by Mrlee on 2017. 10. 16..
+//  Created by Mrlee on 2017. 10. 17..
 //  Copyright © 2017년 Mrlee. All rights reserved.
+
+//  야드길이 변환과 반복입력
 
 import Foundation
 let baseNum: Double = 100
 let centiToInch: Double = 2.54
+let centiToYard: Double = 91.44
 
-func centiToInch(centiInput input: Int) -> Double{
-    return Double(input) / centiToInch
+func printResult(_ num: Double){
+    let numberOfPlaces = 2.0
+    let multiplier = pow(10.0, numberOfPlaces)
+    let rounded = round(num * multiplier) / multiplier
+    print("\(rounded)", terminator: "")
 }
 
-func inchToCenti(inchInput input: Double) -> Int{
-    return Int(input * centiToInch)
+func yardToCenti(yardInput input: Double) -> Double{
+    return input * centiToYard
+}
+
+func CentiToYard(centiInput input:Int) -> Double{
+    return Double(input) / centiToYard
+}
+
+func centiToInch(centiInput input: Double) -> Double{
+    return input / centiToInch
+}
+
+func inchToCenti(inchInput input: Double) -> Double{
+    return input * centiToInch
 }
 
 func meterToCenti(meterInput input: Double) -> Int{
@@ -25,76 +43,116 @@ func centiToMeter(centiInput input: Int) -> Double{
     return Double(input) / baseNum
 }
 
-func sperateInputCM(str input: String) -> Int{
-    var inputVar = input
-    let range = inputVar.index(inputVar.endIndex, offsetBy: -2)..<inputVar.endIndex
-    inputVar.removeSubrange(range)
-    return Int(inputVar)!
-}
-
-func sperateInputM(str input: String) -> Double{
+func sperateInputOneUnit(str input: String) -> (Double, String){
     var inputVar = input
     let range = inputVar.index(inputVar.endIndex, offsetBy: -1)..<inputVar.endIndex
+    let unit = inputVar.substring(with: range)
     inputVar.removeSubrange(range)
-    return Double(inputVar)!
+    return (Double(inputVar)!, unit)
 }
 
-func sperateInputInch(str input: String) -> Double{
+func sperateInputTwoUnit(str input: String) -> (Double, String){
+    var inputVar = input
+    let range = inputVar.index(inputVar.endIndex, offsetBy: -2)..<inputVar.endIndex
+    let unit = inputVar.substring(with: range)
+    inputVar.removeSubrange(range)
+    return (Double(inputVar)!, unit)
+}
+
+func sperateInputFourUnit(str input: String) -> (Double, String){
     var inputVar = input
     let range = inputVar.index(inputVar.endIndex, offsetBy: -4)..<inputVar.endIndex
+    let unit = inputVar.substring(with: range)
     inputVar.removeSubrange(range)
-    return Double(inputVar)!
+    return (Double(inputVar)!, unit)
 }
 
-func convertMeterCentiToInch(str inputString: String){
-    var strArr = inputString.components(separatedBy: " ")
-    if inputString.contains("cm") {
-        print("\(centiToInch(centiInput: sperateInputCM(str: strArr[0]))) inch")
-    }else if inputString.contains("m"){
-        print("\(centiToInch(centiInput: meterToCenti(meterInput: sperateInputM(str: strArr[0])))) inch")
-    }
-}
+//func sperateFunc(str input:String) -> ({
+//
+//}
 
-func convertInchToMeterCenti(str inputString: String){
-    var strArr = inputString.components(separatedBy: " ")
-    if inputString.contains("cm"){
-        print("\(inchToCenti(inchInput: sperateInputInch(str: strArr[0]))) cm")
-    }else if inputString.contains("m"){
-        let num = inchToCenti(inchInput: sperateInputInch(str: strArr[0]))
-        print("\(centiToMeter(centiInput: num)) m")
-    }
-}
 func converFunc(str inputString: String) -> Bool{
-    var strArr = inputString.components(separatedBy: " ")
-    if strArr.count > 1{
+    let strArr = inputString.components(separatedBy: " ")
+    var fromUnit = ""
+    var toUnit = ""
+    var num: Double = 0
+    var result: Double = 0
+    if  strArr.count > 1{
         if strArr[0].contains("cm") {
-            convertMeterCentiToInch(str: inputString)
-            return true
+            (num, fromUnit) = sperateInputTwoUnit(str: strArr[0])
+            toUnit = strArr[1]
         }else if strArr[0].contains("m"){
-            convertMeterCentiToInch(str: inputString)
-            return true
-        }else if strArr[0].contains("inch") {
-            convertInchToMeterCenti(str: inputString)
-            return true
+            (num, fromUnit) = sperateInputOneUnit(str: strArr[0])
+            toUnit = strArr[1]
+        }else if strArr[0].contains("inch") || strArr[0].contains("yard"){
+            (num, fromUnit) = sperateInputFourUnit(str: strArr[0])
+            toUnit = strArr[1]
         }
-    }else{
-        if strArr[0].contains("cm") {
-            print("\(centiToMeter(centiInput: sperateInputCM(str: inputString)))m")
-            return true
-        }else if strArr[0].contains("m"){
-            print("\(meterToCenti(meterInput: sperateInputM(str: inputString)))cm")
-            return true
-        }else{
-            print("지원하지 않는 단위입니다.")
-            return false
+        
+        let unitInfo: (from: String, to: String) = (fromUnit, toUnit)
+        switch unitInfo {
+        case ("cm", "inch"):
+            result = centiToInch(centiInput: num)
+            printResult(result)
+            print(" inch")
+            break
+        case ("m", "inch"):
+            result = centiToInch(centiInput: Double(meterToCenti(meterInput: num)))
+            printResult(result)
+            print(" inch")
+            break
+        case ("inch", "cm"):
+            result = Double(inchToCenti(inchInput: num))
+            printResult(result)
+            print(" cm")
+            break
+        case ("inch", "m"):
+            result = centiToMeter(centiInput: Int(inchToCenti(inchInput: num)))
+            printResult(result)
+            print(" m")
+            break
+        case ("yard", "m"):
+            result = centiToMeter(centiInput: Int(yardToCenti(yardInput: num)))
+            printResult(result)
+            print(" m")
+            break
+        case ("m", "yard"):
+            result = CentiToYard(centiInput: meterToCenti(meterInput: num))
+            printResult(result)
+            print(" yard")
+        default:
+            print("지원하지 않는 단위입니다. 다시입력해주세요.")
+            break
         }
+        return true
+    }else {
+        if inputString.contains("cm") {
+            (num, fromUnit) = sperateInputTwoUnit(str: inputString)
+            result = centiToMeter(centiInput: Int(num))
+            printResult(result)
+            print(" m")
+        }else if inputString.contains("m"){
+            (num, fromUnit) = sperateInputOneUnit(str: inputString)
+            result = Double(meterToCenti(meterInput: num))
+            printResult(result)
+            print(" cm")
+        }else if inputString.contains("yard"){
+            (num, fromUnit) = sperateInputFourUnit(str: inputString)
+            result = centiToMeter(centiInput: Int(yardToCenti(yardInput: num)))
+            printResult(result)
+            print(" m")
+        }
+        return true
     }
-    return true
 }
 
 var result: Bool = true
-repeat{
+while(true){
     print("변환하고 싶은 수치를 입력하세요 : ", terminator: "")
     var inputStr = readLine()
+    
+    if inputStr == "quit" || inputStr == "q"{
+        break
+    }
     result = converFunc(str: inputStr!)
-}while(!result)
+}
