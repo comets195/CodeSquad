d097e540adfb8bf49e672619bccdd3d7790c1006
conalls <comets195@gmail.com>
2017-10-17 12:14:18 +0900

    Level 2. Lecture 2. Lesson 5. Commit 2번

--- UnitConverter/UnitConverter/main.swift
+++ UnitConverter/UnitConverter/main.swift
@@ -4,30 +4,97 @@
 //
 //  Created by Mrlee on 2017. 10. 16..
 //  Copyright © 2017년 Mrlee. All rights reserved.
-//
+
 import Foundation
-
-//길이 단위 변환 함수
 let baseNum: Double = 100
+let centiToInch: Double = 2.54
 
-func convertFunc(str InputString: String){
-    var Input = InputString
-    if Input.hasSuffix("cm"){
-        let range = Input.index(Input.endIndex, offsetBy: -2)..<Input.endIndex
-        Input.removeSubrange(range)
-        let result:Double = Double(Input)!/baseNum
-        print("\(result)m")
-        
-    }else if Input.hasSuffix("m"){
-        let range = Input.index(Input.endIndex, offsetBy: -1)..<Input.endIndex
-        Input.removeSubrange(range)
-        let result:Double = Double(Input)! * baseNum
-        print("\(Int(result))cm")
-    }else{
-        print("Input Error! You must input 180cm or 1.2m")
+func centiToInch(centiInput input: Int) -> Double{
+    return Double(input) / centiToInch
+}
+
+func inchToCenti(inchInput input: Double) -> Int{
+    return Int(input * centiToInch)
+}
+
+func meterToCenti(meterInput input: Double) -> Int{
+    return Int(input * baseNum)
+}
+
+func centiToMeter(centiInput input: Int) -> Double{
+    return Double(input) / baseNum
+}
+
+func sperateInputCM(str input: String) -> Int{
+    var inputVar = input
+    let range = inputVar.index(inputVar.endIndex, offsetBy: -2)..<inputVar.endIndex
+    inputVar.removeSubrange(range)
+    return Int(inputVar)!
+}
+
+func sperateInputM(str input: String) -> Double{
+    var inputVar = input
+    let range = inputVar.index(inputVar.endIndex, offsetBy: -1)..<inputVar.endIndex
+    inputVar.removeSubrange(range)
+    return Double(inputVar)!
+}
+
+func sperateInputInch(str input: String) -> Double{
+    var inputVar = input
+    let range = inputVar.index(inputVar.endIndex, offsetBy: -4)..<inputVar.endIndex
+    inputVar.removeSubrange(range)
+    return Double(inputVar)!
+}
+
+func convertMeterCentiToInch(str inputString: String){
+    var strArr = inputString.components(separatedBy: " ")
+    if inputString.contains("cm") {
+        print("\(centiToInch(centiInput: sperateInputCM(str: strArr[0]))) inch")
+    }else if inputString.contains("m"){
+        print("\(centiToInch(centiInput: meterToCenti(meterInput: sperateInputM(str: strArr[0])))) inch")
     }
 }
 
-print("변환하고 싶은 수치를 입력하세요 : ", terminator: "")
-var inputStr = readLine()
-convertFunc(str: inputStr!)
+func convertInchToMeterCenti(str inputString: String){
+    var strArr = inputString.components(separatedBy: " ")
+    if inputString.contains("cm"){
+        print("\(inchToCenti(inchInput: sperateInputInch(str: strArr[0]))) cm")
+    }else if inputString.contains("m"){
+        let num = inchToCenti(inchInput: sperateInputInch(str: strArr[0]))
+        print("\(centiToMeter(centiInput: num)) m")
+    }
+}
+func converFunc(str inputString: String) -> Bool{
+    var strArr = inputString.components(separatedBy: " ")
+    if strArr.count > 1{
+        if strArr[0].contains("cm") {
+            convertMeterCentiToInch(str: inputString)
+            return true
+        }else if strArr[0].contains("m"){
+            convertMeterCentiToInch(str: inputString)
+            return true
+        }else if strArr[0].contains("inch") {
+            convertInchToMeterCenti(str: inputString)
+            return true
+        }
+    }else{
+        if strArr[0].contains("cm") {
+            print("\(centiToMeter(centiInput: sperateInputCM(str: inputString)))m")
+            return true
+        }else if strArr[0].contains("m"){
+            print("\(meterToCenti(meterInput: sperateInputM(str: inputString)))cm")
+            return true
+        }else{
+            print("지원하지 않는 단위입니다.")
+            return false
+        }
+    }
+    return true
+}
+
+var result: Bool = true
+repeat{
+    print("변환하고 싶은 수치를 입력하세요 : ", terminator: "")
+    var inputStr = readLine()
+    result = converFunc(str: inputStr!)
+}while(!result)
