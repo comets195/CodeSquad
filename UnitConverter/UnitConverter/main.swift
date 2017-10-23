7cbc1ff2f824ff21577a7be80d44a9ee0a717368
conalls <comets195@gmail.com>
2017-10-23 12:34:22 +0900

    edit checkUnit, printResult Function

--- UnitConverter/UnitConverter/main.swift
+++ UnitConverter/UnitConverter/main.swift
@@ -60,6 +60,7 @@ func searchUnitPart(currValWithUnit: String) -> String?{
     for key in lengthUnitDic.keys {
         if currValWithUnit.hasSuffix(key){ return key }
     }
+    //값이 없으면 nil반환
     return nil
 }
 
@@ -78,86 +79,60 @@ func searchDigitPart(valWithFromUnit: String, currUnit: String?) -> Double?{
 func separateInputString(_ inputVal: String) -> Array<String>{
     //전달받은 인수를 " " 공백을 기준으로 나누어 배열에 저장한다.
     let strArr = inputVal.components(separatedBy: " ")
-    
     return strArr
-    //    var convertLength: LengthUnit = LengthUnit(fromUnit: "", toUnit: "", numVal: 0.0)
-    //    //길이 값이 입력되었을때
-    //    if strArr.count > 1 {
-    //        //예외처리할때 한번더 else가서 확인하는게 안좋은데 개선할 수 있는법은..?
-    //        if let unwarpUnit = searchUnitPart(currValWithUnit: strArr[0]),
-    //            let unwarpDigitVal = searchDigitPart(valWithFromUnit: strArr[0], currUnit: unwarpUnit){
-    //             convertLength = LengthUnit(fromUnit: unwarpUnit, toUnit: strArr[1], numVal: unwarpDigitVal)
-    //            return convertLength
-    //        }
-    //    } else {
-    //        if let unwarpUnit = searchUnitPart(currValWithUnit: strArr[0]),
-    //            let unwarpDigitVal = searchDigitPart(valWithFromUnit: strArr[0], currUnit: unwarpUnit){
-    //             convertLength = LengthUnit(fromUnit: unwarpUnit, toUnit: nil, numVal: unwarpDigitVal)
-    //            return convertLength
-    //        }
-    //    }
-    //    return convertLength
 }
 
 func sortUnits(_ inputStr: String?){
     
 }
 
-func convertExecute(_ inputStr: String?) -> numberUnitResult{
+func convertExecute(_ inputStr: String?) -> (numberUnitResult, Bool){
     guard let inputStr = inputStr else {
         print("input Error!")
-        return (0.0, "")
+        return ((0.0, ""), false)
     }
-    //
-    //    let separatedInputStr = separateInputString(inputStr)
-    //    let check: Bool = checkUnit(inputStr: separatedInputStr)
-    //    if check{
-    //        return (0.0, "")
-    //    }
-    //
-    //    //입력단위가 두개라면 실행
-    //    if separatedInputStr.toUnit != nil { return (separatedInputStr.convertInputVal(separatedInputStr), separatedInputStr.toUnit!) }
-    //    else{ return separatedInputStr.convertInputOneVal(separatedInputStr)}
-    
     let separatedInputStr = separateInputString(inputStr) //배열로 저장됨 180cm m
     let fromUnitPart = searchUnitPart(currValWithUnit: separatedInputStr[0]) //입력받은 단위 찾기
-    let separatedDigit = searchDigitPart(valWithFromUnit: separatedInputStr[0], currUnit: fromUnitPart) //입력값에서 숫자 분리
-    //입력받은값이 2개이상일경우
-    if separatedInputStr.count > 1 {
-        let convertUnitDigit: LengthUnit = LengthUnit(fromUnit: fromUnitPart!, toUnit: separatedInputStr[1], numVal: separatedDigit!)
-        return (convertUnitDigit.convertInputVal(convertUnitDigit), separatedInputStr[1])
+    //입력받는 단위 변환 가능한지 체크
+    let checkVal: Bool = checkUnit(inputStr: fromUnitPart)
+    if checkVal{
+        let separatedDigit = searchDigitPart(valWithFromUnit: separatedInputStr[0], currUnit: fromUnitPart) //입력값에서 숫자 분리
+        //입력받은값이 2개이상일경우
+        if separatedInputStr.count > 1 {
+            let convertUnitDigit: LengthUnit = LengthUnit(fromUnit: fromUnitPart!, toUnit: separatedInputStr[1], numVal: separatedDigit!)
+            return ((convertUnitDigit.convertInputVal(convertUnitDigit), separatedInputStr[1]), true)
+        }else{
+            let convertUnitDigit: LengthUnit = LengthUnit(fromUnit: fromUnitPart!, toUnit: nil, numVal: separatedDigit!)
+            return ((convertUnitDigit.convertInputOneVal(convertUnitDigit), fromUnitPart!), true)
+        }
     }else{
-        let convertUnitDigit: LengthUnit = LengthUnit(fromUnit: fromUnitPart!, toUnit: nil, numVal: separatedDigit!)
-        return (convertUnitDigit.convertInputOneVal(convertUnitDigit), fromUnitPart!)
+        return ((0.0, ""), false)
     }
-    
 }
 
-func printResult(inputVal: numberUnitResult) -> Bool{
-    if inputVal.0 == 0.0 {
-        print("\(inputVal.1)")
-    }else{
-        print("\(inputVal.0)\(inputVal.1)")
+func printResult(inputVal: (numberUnitResult, Bool)) -> Bool{
+    if inputVal.1 {
+        print("\(inputVal.0) \(inputVal.0.1)")
     }
     return true
 }
 
-func checkUnit(inputStr: LengthUnit) -> Bool{
-    if inputStr.fromUnit == "" {
-        print("지원하지 않는 단위입니다.")
-        return true
-    }else{
+func checkUnit(inputStr: String?) -> Bool{
+    //inputStr가 nil아닐때 guard문을 지나서 다음 코드가 실행
+    guard inputStr != nil else {
+        print("지원하지 않는 단위입니다. 다시입력하세요.")
         return false
     }
+    return true
 }
 
 var executeVal: Bool = true
 while(executeVal){
     print("변환 하고 싶으신 단위를 입력하세요 : ", terminator: "")
     let inputStr = readLine()
-    
-    
+    if inputStr == "q" || inputStr == "quit"{ break }
     executeVal = printResult(inputVal: convertExecute(inputStr))
     //사용자가 q or quit를 입력하면 함수 종료
-    if inputStr == "q" || inputStr == "quit"{ break }
+    
 }
+
