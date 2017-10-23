5f41f9cb4aa2666580adfd3b3684d76e3633f7df
conalls <comets195@gmail.com>
2017-10-23 21:35:44 +0900

    edit unit convertor

--- UnitConverter/UnitConverter/main.swift
+++ UnitConverter/UnitConverter/main.swift
@@ -12,6 +12,7 @@ enum Units: String {
     case cm
     case g
     case L
+    case error
 }
 
 func roundToPlaces(num: Double) -> Double{
@@ -25,6 +26,7 @@ struct UnitStruct {
     static let lengthUnitDic: UnitsDic = ["cm": 1, "m": 100, "inch": 2.54, "yard": 91.44]
     static let weightUnitDic: UnitsDic = ["g": 1, "kg": 1000, "oz": 28.34, "lb": 435.59]
     static let volumeUnitDic: UnitsDic = ["L": 1, "gal": 3.785, "pt": 0.473, "qt": 0.946]
+    static let error: UnitsDic = ["error": 0]
 }
 
 struct UnitConvert{
@@ -34,16 +36,17 @@ struct UnitConvert{
     
     //fromUnit(분류된 key, 즉 Unit이 들어온걸 분류해야함
     func selectBaseUnit(_ fromUnit: String) -> (Units, UnitsDic) {
-        for key in UnitStruct.lengthUnitDic.keys {
-            if key == fromUnit{ return (Units.cm, UnitStruct.lengthUnitDic) }
+        let key = searchUnitPart(currValWithUnit: fromUnit)
+        switch key.1 {
+        case "lengthUnitDic":
+            return (Units.cm, UnitStruct.lengthUnitDic)
+        case "weightUnitDic":
+            return (Units.g, UnitStruct.weightUnitDic)
+        case "volumeUnitDic":
+            return (Units.L, UnitStruct.volumeUnitDic)
+        default:
+            return (Units.error, UnitStruct.error)
         }
-        for key in UnitStruct.weightUnitDic.keys {
-            if key == fromUnit { return (Units.g, UnitStruct.weightUnitDic) }
-        }
-        for key in UnitStruct.volumeUnitDic.keys {
-            if key == fromUnit { return (Units.L, UnitStruct.volumeUnitDic) }
-        }
-        return (Units.cm, UnitStruct.lengthUnitDic)
     }
     //입력받은 UnitConvert구조체를 처리한다., 단 입력값이 2개여야함(입력단위, 변환하고자 하는 단위)
     func convertInputVal(_ convertUnit: UnitConvert) -> String{
@@ -93,21 +96,21 @@ struct UnitConvert{
 }
 
 //입력받은 단위를 찾아내는 함수
-func searchUnitPart(currValWithUnit: String) -> String?{
+func searchUnitPart(currValWithUnit: String) -> (String?, String){
     //길이단위 딕셔너리에 값이 있는지 확인
     for key in UnitStruct.lengthUnitDic.keys {
-        if currValWithUnit.hasSuffix(key){ return key }
+        if currValWithUnit.hasSuffix(key){ return (key, "lengthUnitDic") }
     }
     //무게단위 딕셔너리에 값이 있는지 확인
     for key in UnitStruct.weightUnitDic.keys {
-        if currValWithUnit.hasSuffix(key){ return key }
+        if currValWithUnit.hasSuffix(key){ return (key, "weightUnitDic") }
     }
     //부피단위 딕셔너리에 값이 있는지 확인
     for key in UnitStruct.volumeUnitDic.keys {
-        if currValWithUnit.hasSuffix(key){ return key }
+        if currValWithUnit.hasSuffix(key){ return (key, "volumeUnitDic") }
     }
     //값이 없으면 nil반환
-    return nil
+    return (nil, "")
 }
 
 //입력값중 숫자값을 거르는 함수
@@ -141,20 +144,20 @@ func convertExecute(_ inputStr: String?) -> (String, Int, Bool){
     let separatedInputStr = separateInputString(inputStr) //배열로 저장됨 180cm m,inch
     let fromUnitPart = searchUnitPart(currValWithUnit: separatedInputStr[0]) //입력받은 단위 찾기
     //입력받는 단위 변환 가능한지 체크
-    let checkVal: Bool = checkUnit(inputStr: fromUnitPart)
+    let checkVal: Bool = checkUnit(inputStr: fromUnitPart.0)
     if checkVal{
-        let separatedDigit = searchDigitPart(valWithFromUnit: separatedInputStr[0], currUnit: fromUnitPart) //입력값에서 숫자 분리
+        let separatedDigit = searchDigitPart(valWithFromUnit: separatedInputStr[0], currUnit: fromUnitPart.0) //입력값에서 숫자 분리
         //입력받은값이 2개이상일경우
         if separatedInputStr.count > 1 {
             let separateToUnit = separateInputStringToUnit(separatedInputStr[1])
-            let convertUnitDigit: UnitConvert = UnitConvert(fromUnit: fromUnitPart!,
+            let convertUnitDigit: UnitConvert = UnitConvert(fromUnit: fromUnitPart.0!,
                                                             toUnit: separateToUnit,
                                                             numVal: separatedDigit!)
             let converResult = convertUnitDigit.convertInputVal(convertUnitDigit)
             return (converResult, separateToUnit.count, true)
         }else{
             //입력받은값이 1개일경우
-            let convertUnitDigit: UnitConvert = UnitConvert(fromUnit: fromUnitPart!,
+            let convertUnitDigit: UnitConvert = UnitConvert(fromUnit: fromUnitPart.0!,
                                                             toUnit: [nil],
                                                             numVal: separatedDigit!)
             let convertResult = convertUnitDigit.convertInputOneVal(convertUnitDigit)
